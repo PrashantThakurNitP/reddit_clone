e94a6c0c9afeb19de4fb92d073849e02491ae131
PrashantThakurNitP <prashant09.thakur@gmail.com>
2020-12-23 19:03:37 +0530

    adding post and viewing post only from webpage

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,9 +4,12 @@ from .models import Post#import post models
 #there are different kind of serializer
 #ModelSerializer convert model into json
 class PostSerializer(serializers.ModelSerializer):
+	poster=serializers.ReadOnlyField(source='poster.username')
+	poster_id=serializers.ReadOnlyField(source='poster.id')
+	#we make the field read only field and we have specified source from where data will come
 	class Meta:
 		model=Post
-		fields=['id','title','url','poster','created']
+		fields=['id','title','url','poster','poster_id','created']
 		#id is explicitly listed as field but it is included with eveny django model
 		#this is saying if someone want to get post from database it can come out as model 
 		#go through serializer  and eventually convert into json
--- posts/views.py
+++ posts/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 #ability to list out all different post that are saved inside database
-from rest_framework import generics
+from rest_framework import generics,permissions
 from .models import Post
 from .serializers import PostSerializer
-class PostList(generics.ListAPIView):#class based view that list out all informations
+class PostList(generics.ListCreateAPIView):#class based view that list out all informations
 	queryset=Post.objects.all()
 	#now specify what serilizer to be used for this view
 	serializer_class=PostSerializer
+	# make sure that only logged in user can make call to api
+	#permission_classes=[permissions.IsAuthenticated] # this tell Authentication credentials were not provided
+		
+	
+	permission_classes=[permissions.IsAuthenticatedOrReadOnly]# it allow to read but not write if not authenticated
+	def perform_create(self,serializer):#name of this function must be same
+		serializer.save(poster=self.request.user)
